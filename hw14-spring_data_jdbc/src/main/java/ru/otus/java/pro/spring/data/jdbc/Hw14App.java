c7ee4ba0d5df4345da554808c9b3e950448a4dca
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2024-12-19 14:16:58 +0300

    Homework-14 completed

---
+++ hw14-spring_data_jdbc/src/main/java/ru/otus/java/pro/spring/data/jdbc/Hw14App.java
@@ -0,0 +1,14 @@
+package ru.otus.java.pro.spring.data.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Hw14App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw14App.class, args);
+	}
+
+}
